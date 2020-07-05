4c6520fbc486d66151fed41af2c80c33aa2ef2d4
Daniel Andrade <andrade.daniel111@gmail.com>
2020-07-04 17:57:17 -0700

    trying to fix form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -2,38 +2,38 @@ import React, { Component } from "react"
 import { Button, Form, FormGroup, Input } from "reactstrap"
 import { navigateTo } from "gatsby-link"
 
-// function encode(data) {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&")
-// }
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
 
 export class ContactForm extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {}
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  // handleChange = e => {
-  //   this.setState({ [e.target.name]: e.target.value })
-  // }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-  // handleSubmit = e => {
-  //   e.preventDefault()
-  //   const form = e.target
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": form.getAttribute("name"),
-  //       ...this.state,
-  //     }),
-  //   })
-  //     .then(() => navigateTo("/"))
-  //     .catch(error => alert(error))
-  //   // .then(() => navigateTo(form.getAttribute("action")))
-  //   // .catch(error => alert(error));
-  // }
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      // .then(() => navigateTo("/"))
+      // .catch(error => alert(error))
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
 
   render() {
     return (
